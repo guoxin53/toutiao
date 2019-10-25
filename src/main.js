8351680e00a0f18069aaa42d646854aef75f9406
GuoXin-99 <1072560418@qq.com>
2019-10-25 21:24:26 +0800

    项目初始完毕

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+// 根实例
 new Vue({
   render: h => h(App)
 }).$mount('#app')
+// main.js 作用 是 入口文件
+// 职责1：创建一个根实例
+// 职责2. 负责全局范围的依赖导入
