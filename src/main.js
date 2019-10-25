fc2c46f97a6ae75f1e3dfa04fa3bbf6c0ea65094
GuoXin-99 <1072560418@qq.com>
2019-10-25 22:43:15 +0800

    使用vue-router

--- src/main.js
+++ src/main.js
@@ -3,12 +3,16 @@ import App from './App.vue'
 
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+import router from '@router' // 引入路由模块  会默认去找index.js
+
 Vue.use(ElementUI) // 全局安装了ElementUI
 
 Vue.config.productionTip = false
 
 // 根实例
 new Vue({
+  router, // 挂载
   render: h => h(App)
 }).$mount('#app')
 // main.js 作用 是 入口文件
---
+++ src/router/index.js
@@ -0,0 +1,12 @@
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+
+Vue.use(VueRouter) // 全局使用
+
+// 实例化
+export default new VueRouter({
+  // 匹配路由规则
+  routes: [
+
+  ]
+})
