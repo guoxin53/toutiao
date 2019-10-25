cfe13bcd7358357ac220f0b5ba4423024783fe96
GuoXin-99 <1072560418@qq.com>
2019-10-25 22:17:31 +0800

    使用饿了么UI框架

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="app">122</div>
+  <div id="app">
+    123
+    <el-button type="primary">主要按钮</el-button>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI) // 全局安装了ElementUI
+
 Vue.config.productionTip = false
 
 // 根实例
